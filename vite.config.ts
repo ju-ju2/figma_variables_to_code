9a035931a81e4e93f6c1fc52f36c28b2fe23f8df
한주연 <hjy2859@naver.com>
2025-07-11 01:43:41 +0900

    fix: tailwind 스타일 안먹는 이슈 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   root: "./src/ui",
-  plugins: [react(), viteSingleFile(), tailwindcss()],
+  plugins: [react(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
