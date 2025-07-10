053e320c586465cca7c02111b80cd6ff01489ac6
한주연 <hjy2859@naver.com>
2025-07-11 00:29:22 +0900

    feat: shadcn 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   root: "./src/ui",
-  plugins: [react(), viteSingleFile()],
+  plugins: [react(), viteSingleFile(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     target: "esnext",
     minify: "esbuild",
