7af44ea3ee6ed3db071a199a3633adc11f1488f2
한주연 <hjy2859@naver.com>
2025-07-11 21:48:04 +0900

    chore: fontStyles 만 받도록 주석 처리

--- src/code/localStylesScss.ts
+++ src/code/localStylesScss.ts
@@ -1,86 +1,87 @@
-import { rgbaToHex } from "./shared";
+// import { rgbaToHex } from "./shared";
 
 type LineHeightType = {
   readonly value: number;
   readonly unit: "PIXELS" | "PERCENT";
 };
 
-function replaceLowerCaseName(name: string) {
-  return name.replace(/\s+/g, "-").replace(/\//g, "-").toLowerCase();
-}
+// function replaceLowerCaseName(name: string) {
+//   return name.replace(/\s+/g, "-").replace(/\//g, "-").toLowerCase();
+// }
 
-const convertToColorCode = (color: RGBA | RGB) => {
-  const _color = { r: color.r * 255, b: color.b * 255, g: color.g * 255 };
-  const beautifiedColor =
-    "a" in color
-      ? rgbaToHex(_color.r, _color.g, _color.b, color.a)
-      : rgbaToHex(_color.r, _color.g, _color.b);
+// const convertToColorCode = (color: RGBA | RGB) => {
+//   const _color = { r: color.r * 255, b: color.b * 255, g: color.g * 255 };
+//   const beautifiedColor =
+//     "a" in color
+//       ? rgbaToHex(_color.r, _color.g, _color.b, color.a)
+//       : rgbaToHex(_color.r, _color.g, _color.b);
 
-  return beautifiedColor;
-};
+//   return beautifiedColor;
+// };
 
-const getColor = async () => {
-  const paintStyles = await figma.getLocalPaintStylesAsync();
-  const filteredPaintStyles = paintStyles.filter((paintStyle) => {
-    const color = paintStyle.paints[0] as SolidPaint;
-    return color.type === "SOLID";
-  });
+// const getColor = async () => {
+//   const paintStyles = await figma.getLocalPaintStylesAsync();
+//   const filteredPaintStyles = paintStyles.filter((paintStyle) => {
+//     const color = paintStyle.paints[0] as SolidPaint;
+//     return color.type === "SOLID";
+//   });
 
-  return filteredPaintStyles.map((paintStyle) => {
-    const paint = paintStyle.paints[0] as SolidPaint;
+//   return filteredPaintStyles.map((paintStyle) => {
+//     const paint = paintStyle.paints[0] as SolidPaint;
 
-    const hex = convertToColorCode({ ...paint.color, a: paint.opacity });
-    return `$${replaceLowerCaseName(paintStyle.name)}: ${hex};`;
-  });
-};
+//     const hex = convertToColorCode({ ...paint.color, a: paint.opacity });
+//     return `$${replaceLowerCaseName(paintStyle.name)}: ${hex};`;
+//   });
+// };
 
-const getShadows = async () => {
-  const effectStyles = await figma.getLocalEffectStylesAsync();
+// const getShadows = async () => {
+//   const effectStyles = await figma.getLocalEffectStylesAsync();
 
-  return effectStyles
-    .filter((effectStyle) => {
-      const effect = effectStyle.effects[0];
-      return effect.type === "DROP_SHADOW" || effect.type === "INNER_SHADOW";
-    })
-    .map((effectStyle) => {
-      const effect = effectStyle.effects[0];
+//   return effectStyles
+//     .filter((effectStyle) => {
+//       const effect = effectStyle.effects[0];
+//       return effect.type === "DROP_SHADOW" || effect.type === "INNER_SHADOW";
+//     })
+//     .map((effectStyle) => {
+//       const effect = effectStyle.effects[0];
 
-      if (effect.type !== "DROP_SHADOW" && effect.type !== "INNER_SHADOW") {
-        return "";
-      }
+//       if (effect.type !== "DROP_SHADOW" && effect.type !== "INNER_SHADOW") {
+//         return "";
+//       }
 
-      return `$${replaceLowerCaseName(effectStyle.name)}:${effect.offset.x}px ${
-        effect.offset.y
-      }px ${effect.radius}px ${convertToColorCode(effect.color)};`;
-    });
-};
+//       return `$${replaceLowerCaseName(effectStyle.name)}:${effect.offset.x}px ${
+//         effect.offset.y
+//       }px ${effect.radius}px ${convertToColorCode(effect.color)};`;
+//     });
+// };
 
-const getBlurs = async () => {
-  const effectStyles = await figma.getLocalEffectStylesAsync();
+// const getBlurs = async () => {
+//   const effectStyles = await figma.getLocalEffectStylesAsync();
 
-  return effectStyles
-    .filter((effectStyle) => {
-      const effect = effectStyle.effects[0];
-      return effect.type === "BACKGROUND_BLUR" || effect.type === "LAYER_BLUR";
-    })
-    .map((effectStyle) => {
-      const effect = effectStyle.effects[0];
+//   return effectStyles
+//     .filter((effectStyle) => {
+//       const effect = effectStyle.effects[0];
+//       return effect.type === "BACKGROUND_BLUR" || effect.type === "LAYER_BLUR";
+//     })
+//     .map((effectStyle) => {
+//       const effect = effectStyle.effects[0];
 
-      if (effect.type !== "BACKGROUND_BLUR" && effect.type !== "LAYER_BLUR") {
-        return "";
-      }
+//       if (effect.type !== "BACKGROUND_BLUR" && effect.type !== "LAYER_BLUR") {
+//         return "";
+//       }
 
-      return `$${replaceLowerCaseName(effectStyle.name)}: blur(${
-        effect.radius
-      }px);`;
-    });
-};
+//       return `$${replaceLowerCaseName(effectStyle.name)}: blur(${
+//         effect.radius
+//       }px);`;
+//     });
+// };
 
 async function getFontStyles() {
   const textStyles = await figma.getLocalTextStylesAsync();
 
   return textStyles.map((textStyle) => {
-    const className = textStyle.name.replace(/\d+/g, "").replace("/", "");
+    const className = textStyle.name.split("/")[0];
+
     const getFontWeight = () => {
       const fontWeight = textStyle.fontName.style.toLowerCase();
       if (fontWeight === "bold") {
@@ -102,17 +103,20 @@ async function getFontStyles() {
   "${className}-line-height": ${
       (textStyle.lineHeight as LineHeightType).value
     }px,
-  "${className}-letter-spacing": ${textStyle.letterSpacing.value}px,
+  "${className}-letter-spacing": ${parseFloat(
+      textStyle.letterSpacing.value.toFixed(2)
+    )}%,
 `;
   });
 }
 
 export const getLocalStyles = async () => {
-  const colors = await getColor();
-  const shadows = await getShadows();
-  const blurs = await getBlurs();
+  // const colors = await getColor();
+  // const shadows = await getShadows();
+  // const blurs = await getBlurs();
   const fontStyles = await getFontStyles();
 
-  const localStyles = [...colors, ...shadows, ...blurs, ...fontStyles];
+  // const localStyles = [...colors, ...shadows, ...blurs, ...fontStyles];
+  const localStyles = [...fontStyles];
   return localStyles;
 };
